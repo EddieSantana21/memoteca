3154dc073699b4b58926585c4af1db4562855d6e
EddieSantana21 <86499043+EddieSantana21@users.noreply.github.com>
2023-04-10 11:10:10 -0300

    Passando informações atraves do @input

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
+  @Input() pensamento = {
+    conteudo: 'I love Angular',
+    autoria: 'Eddie',
+    modelo: 'modelo3'
+  }
   constructor() { }
 
   ngOnInit(): void {
